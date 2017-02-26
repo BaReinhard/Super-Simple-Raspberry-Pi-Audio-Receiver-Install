216b38706488d60e3e36c1137b29b8fd47349405
Brett Reinhard <brett@reinhards.us>
2017-02-26 08:35:10 -0800

    Update lirc_config.sh

--- lirc_config.sh
+++ lirc_config.sh
@@ -197,10 +197,7 @@ _IP=$(hostname -I) || true
 if [ "$_IP" ]; then
   printf "My IP address is %s\n" "$_IP"
 fi
-# Disable Console Sleep
-setterm -powerdown 0
-# Faster boot
-dmseg --console-off
+
 /home/pi/shScripts/firstrun.sh&
 exit 0
 EOT
