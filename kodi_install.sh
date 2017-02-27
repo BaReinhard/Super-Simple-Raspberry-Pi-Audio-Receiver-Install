d655f4ec8a2ef25a8b63840e29d6f1286df1c649
Brett Reinhard <brett@reinhards.us>
2017-02-27 06:02:46 -0800

    Update kodi_install.sh

--- kodi_install.sh
+++ kodi_install.sh
@@ -13,21 +13,8 @@ function tst {
 
 # Install Dependencies
 tst apt-get install kodi -y
-patch /etc/default/kodi << EOT
-@@ -1,5 +1,5 @@
- # Set this to 1 to enable startup
--ENABLED=0
-+ENABLED=1
- 
- # The user to run Kodi as
- USER=kodi
-
-EOT
-
-usermod -a -G input kodi
-echo gpu_mem=256 >> /boot/config.txt
-
-cd /home/kodi
-wget http://fusion.tvaddons.ag/begin-here/plugin.program.indigo-1.0.2.zip
+# Download TV-Addons Zip for easy installation
+tst cd /home/kodi
+tst wget http://fusion.tvaddons.ag/begin-here/plugin.program.indigo-1.0.2.zip
 
 exit 0
