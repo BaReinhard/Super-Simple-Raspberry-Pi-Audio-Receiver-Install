310745ed665463bb8e2e96350471a97ed2a51aa4
Brett Reinhard <brettreinhard@gmail.com>
2017-12-03 20:52:01 -0800

    setup /tmp/snapfifo as PA sink
    install and make snapcast client and server, setup PA sink for /tmp/snapfifo

--- snapcast_install.sh
+++ snapcast_install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+SNAP="SNAP"
+while [ $SNAP != "s" ] && [ $SNAP != "c" ] && [ $SNAP != "b" ];
+do
+    read -p "Install this as a SnapCast Server(s), Client(c), and Both (b): (s/c/b)" SNAP
+done
 if [ $SUDO_USER ]; then echo "needs to be run as non-root user";exit 1; else user=`whoami`; fi
 #--------------------------------------------------------------------
 function tst {
@@ -18,12 +23,22 @@ tst sudo apt-get install libasound2-dev libvorbisidec-dev libvorbis-dev libflac-
 tst cd $SNAP_DIR
 tst make
 
-# IF CLIENT OR SERVER IMPLEMENT LATER
-tst sudo make installserver
-tst sudo make installclient
 
+if [ $SNAP = "s" ]
+then
+    tst sudo make installserver
+elif [ $SNAP = "c" ]
+    tst sudo make installclient
+elif [ $SNAP = "b" ]
+    tst sudo make installserver
+    tst sudo make installclient
+fi
 # LATER CREATE Config
 
 # Change Pulse to output to snapcast
 
 # Change Shairport to output to snapcast, shairport conf output /tmp/snapfifo
+
+sudo cat << EOT >> /etc/pulse/system.pa
+load-module module-pipe-sink file=/tmp/snapfifo sink_name=Snapcast 
+EOT
