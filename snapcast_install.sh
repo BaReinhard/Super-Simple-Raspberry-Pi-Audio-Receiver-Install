981cc519439f087cdfc7cd4eebaee404450c5668
Brett Reinhard <brettreinhard@gmail.com>
2017-12-03 20:03:09 -0800

    Updated snap install, still testing

--- snapcast_install.sh
+++ snapcast_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+if [ $SUDO_USER ]; then echo "needs to be run as non-root user";exit 1; else user=`whoami`; fi
 #--------------------------------------------------------------------
 function tst {
     echo "===> Executing: $*"
@@ -9,34 +9,21 @@ function tst {
     fi
 }
 #--------------------------------------------------------------------
+SNAP_DIR=/home/${user}/snapcast
+tst git clone https://github.com/badaix/snapcast.git
+tst cd $SNAP_DIR/externals
+tst git submodule update --init --recursive
+tst sudo apt-get install build-essential -y
+tst sudo apt-get install libasound2-dev libvorbisidec-dev libvorbis-dev libflac-dev alsa-utils libavahi-client-dev avahi-daemon -y
+tst cd $SNAP_DIR
+tst make
 
-# Install docker
-tst curl -sSL https://get.docker.com | sh
-tst sudo apt-get update
-tst sudo apt-get install apt-transport-https \
-                       ca-certificates \
-                       software-properties-common
-tst curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
-tst apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
-tst echo "deb https://apt.dockerproject.org/repo/ raspbian-jessie main" >> /etc/apt/sources.list
-tst sudo apt-get update
-tst sudo apt-get -y install docker-engine
-tst git clone https://github.com/nickaknudson/snapcast-pi.git ~/snapcast-pi && cd snapcast-pi
-tst docker run -it -v $(pwd):/home/buildroot/snapcast-pi -w /home/buildroot/snapcast-pi nickaknudson/buildroot /bin/bash
-BUILDROOT_VERSION=2016.11.2
-tst git clone --branch $BUILDROOT_VERSION --depth=1 git://git.buildroot.net/buildroot ~/buildroot
-tst git clone --depth=1 https://github.com/badaix/snapcast.git ~/snapcast
+# IF CLIENT OR SERVER IMPLEMENT LATER
+tst sudo make installserver
+tst sudo make installclient
 
+# LATER CREATE Config
 
-# if pi 0
-tst make O=~/buildroot-output BR2_DL_DIR=~/buildroot-dl BR2_EXTERNAL=~/snapcast/buildroot:~/snapcast-pi/buildroot-external -C ~/buildroot snapcastpi0_defconfig
+# Change Pulse to output to snapcast
 
-
-# if pi 3
-
-
-tst make O=~/buildroot-output BR2_DL_DIR=~/buildroot-dl BR2_EXTERNAL=~/snapcast/buildroot:~/snapcast-pi/buildroot-external -C ~/buildroot menuconfig
-
-
-
-### PICK FROM FLASHING
+# Change Shairport to output to snapcast, shairport conf output /tmp/snapfifo
