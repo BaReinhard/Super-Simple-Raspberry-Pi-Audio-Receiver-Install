2a78f9129fda97b875d561b59be54f777d7db7f2
Brett Reinhard <brettreinhard@gmail.com>
2017-12-15 21:20:52 -0800

    Update logging

--- ap_install.sh
+++ ap_install.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-#--------------------------------------------------------------------
-function tst {
-    echo "===> Executing: $*"
-    if ! $*; then
-        echo "Exiting script due to error from: $*"
-        exit 1
-    fi	
-}
-#--------------------------------------------------------------------
-
-
+if [ -z "$exc" ]
+then
+    source functions.sh
+    source dependencies.sh
+fi
 # Install Dependencies
-tst apt-get install hostapd isc-dhcp-server -y
-
-
+for _dep in ${AP_DEPS[@]}; do
+    apt_install $_dep;
+done
 
 
