268631373961a4306556bb89a1ff3c9c68ba3df4
Brett Reinhard <brettreinhard@gmail.com>
2017-12-15 20:53:57 -0800

    Updated logging and error handling

--- airplay_install.sh
+++ airplay_install.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
 
-#--------------------------------------------------------------------
-function tst {
-    echo "===> Executing: $*"
-    if ! $*; then
-        echo "Exiting script due to error from: $*"
-        exit 1
-    fi
-}
-#--------------------------------------------------------------------
+
+if [ -z "$exc" ]
+then
+    source functions.sh
+    source dependencies.sh
+fi
+
+# Install Dependencies
+for _dep in ${AIRPLAY_DEPS[@]}; do
+    apt_install $_dep;
+done
 
 # Install shairplay-sync from source
 
-tst apt-get -y install build-essential git autoconf automake libtool libdaemon-dev libasound2-dev libpopt-dev libconfig-dev avahi-daemon libavahi-client-dev libssl-dev libpolarssl-dev libsoxr-dev
-tst cd /$HOME
-tst git clone https://github.com/mikebrady/shairport-sync.git
-tst cd shairport-sync
-tst autoreconf -i -f
-tst ./configure --sysconfdir=/etc --with-stdout --with-pipe --with-alsa --with-avahi --with-ssl=openssl --with-metadata --with-soxr --with-systemd
-sleep 1
-tst make
-sleep 1
-getent group shairport-sync &>/dev/null || sudo groupadd -r shairport-sync >/dev/null
-getent passwd shairport-sync &> /dev/null || sudo useradd -r -M -g shairport-sync -s /usr/bin/nologin -G audio shairport-sync >/dev/null
-sleep 1
-tst make install
-sleep 1
-tst systemctl enable shairport-sync
-
-echo "Done! You should reboot now"
+exc cd ~
+exc git clone https://github.com/mikebrady/shairport-sync.git
+exc cd shairport-sync
+exc autoreconf -i -f
+exc ./configure --sysconfdir=/etc --with-stdout --with-pipe --with-alsa --with-avahi --with-ssl=openssl --with-metadata --with-soxr --with-systemd
+exc make
+exc getent group shairport-sync &>/dev/null || sudo groupadd -r shairport-sync >/dev/null
+exc getent passwd shairport-sync &> /dev/null || sudo useradd -r -M -g shairport-sync -s /usr/bin/nologin -G audio shairport-sync >/dev/null
+exc make install
+exc systemctl enable shairport-sync
