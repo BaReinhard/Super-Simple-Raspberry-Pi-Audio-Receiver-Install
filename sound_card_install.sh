55c5c992b70acccfc74fe573f84d16c14d81982b
Brett Reinhard <brettreinhard@gmail.com>
2017-12-18 07:21:51 -0800

    removed redundancy

--- sound_card_install.sh
+++ sound_card_install.sh
@@ -42,10 +42,6 @@ then
 		esac
 	done
 fi
-if [ $SoundCard = "0" ]
-then
-echo "options snd_bcm2835 index=0" | sudo tee -a /etc/modprobe.d/alsa-base.conf
-
 sudo cat << EOT >> /etc/asound.conf
 pcm.pulse {
     type pulse
@@ -82,7 +78,7 @@ sudo cat <<EOT >> /boot/config.txt
 # Enable iqaudio-dac
 #dtoverlay=iqaudio-dac
 
-# Enable iqaudio-dat+
+# Enable iqaudio-dac+
 #dtoverlay=iqaudio-dacplus
 
 # Enable iqaudio-DigiAMP+
@@ -97,601 +93,76 @@ sudo cat <<EOT >> /boot/config.txt
 # Enable JustBoom Digi Cards
 #dtoverlay=justboom-digi
 EOT
+if [ "$SoundCard" != "0" ]
+then
+    sed -i "s/dtparam=audio=on/#dtparam=audio=on/" /boot/config.txt
+fi
+if [ "$SoundCard" = "0" ]
+then
+echo "options snd_bcm2835 index=0" | sudo tee -a /etc/modprobe.d/alsa-base.conf
+
 elif [ "$SoundCard" = "1" ]
 then
-exc sudo cat <<EOT >> /etc/modprobe.d/alsa-base.conf
-options snd_bcm2835 index=0
-EOT
-exc sudo cat <<EOT >> /etc/asound.conf
-pcm.pulse {
-    type pulse
-    card 1
-}
-ctl.pulse {
-    type pulse
-    card 1
-}
 
-pcm.!default {
-    type hw
-    card 1
-}
-ctl.!default {
-    type hw
-    card 1
-}
-EOT
-sudo cat << EOT >>/boot/config.txt
-# Enable HiFiberry Amp
-#dtoverlay=hifiberry-amp
-
+cat << EOT >>/boot/config.txt
 # Enable HiFiberry DAC Light
 dtoverlay=hifiberry-dac
-
-# Enable HiFiberry DAC Standard/Pro
-#dtoverlay=hifiberry-dacplus
-
-# Enable HiFiberry Digi
-#dtoverlay=hifiberry-digi
-
-# Enable iqaudio-dac
-#dtoverlay=iqaudio-dac
-
-# Enable iqaudio-dat+
-#dtoverlay=iqaudio-dacplus
-
-# Enable iqaudio-DigiAMP+
-#dtoverlay=iqaudio-dacplus,unmute_amp
-
-# Enable iqaudio-Digi+
-#dtoverlay=iqaudio-digi-wm8804-audio
-
-# Enable JustBoom DAC and AMP Card
-#dtoverlay=justboom-dac
-
-# Enable JustBoom Digi Cards
-#dtoverlay=justboom-digi
 EOT
 elif [ "$SoundCard" = "2" ]
 then
-echo "options snd_bcm2835 index=0" | sudo tee -a /etc/modprobe.d/alsa-base.conf
-sudo cat << EOT >> /etc/asound.conf
-pcm.pulse {
-    type pulse
-    card 1
-}
-ctl.pulse {
-    type pulse
-    card 1
-}
-
-pcm.!default {
-    type hw
-    card 1
-}
-ctl.!default {
-    type hw
-    card 1
-}
-EOT
-sudo cat <<EOT >>/boot/config.txt
-# Enable HiFiberry Amp
-#dtoverlay=hifiberry-amp
-
-# Enable HiFiberry DAC Light
-#dtoverlay=hifiberry-dac
-
+cat <<EOT >>/boot/config.txt
 # Enable HiFiberry DAC Standard/Pro
 dtoverlay=hifiberry-dacplus
-
-# Enable HiFiberry Digi
-#dtoverlay=hifiberry-digi
-
-# Enable iqaudio-dac
-#dtoverlay=iqaudio-dac
-
-# Enable iqaudio-dat+
-#dtoverlay=iqaudio-dacplus
-
-# Enable iqaudio-DigiAMP+
-#dtoverlay=iqaudio-dacplus,unmute_amp
-
-# Enable iqaudio-Digi+
-#dtoverlay=iqaudio-digi-wm8804-audio
-
-# Enable JustBoom DAC and AMP Card
-#dtoverlay=justboom-dac
-
-# Enable JustBoom Digi Cards
-#dtoverlay=justboom-digi
 EOT
 elif [ "$SoundCard" = "3" ]
 then
-echo "options snd_bcm2835 index=0" | sudo tee -a /etc/modprobe.d/alsa-base.conf
-sudo cat <<EOT >> /etc/asound.conf
-pcm.pulse {
-    type pulse
-    card 1
-}
-ctl.pulse {
-    type pulse
-    card 1
-}
-
-pcm.!default {
-    type hw
-    card 1
-}
-ctl.!default {
-    type hw
-    card 1
-}
-EOT
-sudo cat <<EOT >>/boot/config.txt
-# Enable HiFiberry Amp
-#dtoverlay=hifiberry-amp
-
-# Enable HiFiberry DAC Light
-#dtoverlay=hifiberry-dac
-
-# Enable HiFiberry DAC Standard/Pro
-#dtoverlay=hifiberry-dacplus
-
-
+cat <<EOT >>/boot/config.txt
 # Enable HiFiberry Digi
 dtoverlay=hifiberry-digi
-
-# Enable iqaudio-dac
-#dtoverlay=iqaudio-dac
-
-# Enable iqaudio-dat+
-#dtoverlay=iqaudio-dacplus
-
-# Enable iqaudio-DigiAMP+
-#dtoverlay=iqaudio-dacplus,unmute_amp
-
-# Enable iqaudio-Digi+
-#dtoverlay=iqaudio-digi-wm8804-audio
-
-# Enable JustBoom DAC and AMP Card
-#dtoverlay=justboom-dac
-
-# Enable JustBoom Digi Cards
-#dtoverlay=justboom-digi
 EOT
 
 elif [ "$SoundCard" = "4" ]
 then
-echo "options snd_bcm2835 index=0" | sudo tee -a /etc/modprobe.d/alsa-base.conf
-sudo cat <<EOT >> /etc/asound.conf
-pcm.pulse {
-    type pulse
-    card 1
-}
-ctl.pulse {
-    type pulse
-    card 1
-}
-
-pcm.!default {
-    type hw
-    card 1
-}
-ctl.!default {
-    type hw
-    card 1
-}
-EOT
-sudo cat <<EOT >>/boot/config.txt
+cat <<EOT >>/boot/config.txt
 # Enable HiFiberry Amp
 dtoverlay=hifiberry-amp
-
-# Enable HiFiberry DAC Light
-#dtoverlay=hifiberry-dac
-
-# Enable HiFiberry DAC Standard/Pro
-#dtoverlay=hifiberry-dacplus
-
-
-# Enable HiFiberry Digi
-#dtoverlay=hifiberry-digi
-
-# Enable iqaudio-dac
-#dtoverlay=iqaudio-dac
-
-# Enable iqaudio-dat+
-#dtoverlay=iqaudio-dacplus
-
-# Enable iqaudio-DigiAMP+
-#dtoverlay=iqaudio-dacplus,unmute_amp
-
-# Enable iqaudio-Digi+
-#dtoverlay=iqaudio-digi-wm8804-audio
-
-# Enable JustBoom DAC and AMP Card
-#dtoverlay=justboom-dac
-
-# Enable JustBoom Digi Cards
-#dtoverlay=justboom-digi
 EOT
 elif [ "$SoundCard" = "5" ]
 then
-echo "options snd_bcm2835 index=0" | sudo tee -a /etc/modprobe.d/alsa-base.conf
-sudo cat <<EOT >> /etc/asound.conf
-pcm.pulse {
-    type pulse
-    card 1
-}
-ctl.pulse {
-    type pulse
-    card 1
-}
-
-pcm.!default {
-    type hw
-    card 1
-}
-ctl.!default {
-    type hw
-    card 1
-}
-EOT
-sudo cat <<EOT >>/boot/config.txt
-# Enable HiFiberry Amp
-#dtoverlay=hifiberry-amp
-
-# Enable HiFiberry DAC Light
-#dtoverlay=hifiberry-dac
-
-# Enable HiFiberry DAC Standard/Pro
-#dtoverlay=hifiberry-dacplus
-
-
-# Enable HiFiberry Digi
-#dtoverlay=hifiberry-digi
-
+cat <<EOT >>/boot/config.txt
 # Enable iqaudio-dac
 dtoverlay=iqaudio-dac
-
-# Enable iqaudio-dat+
-#dtoverlay=iqaudio-dacplus
-
-# Enable iqaudio-DigiAMP+
-#dtoverlay=iqaudio-dacplus,unmute_amp
-
-# Enable iqaudio-Digi+
-#dtoverlay=iqaudio-digi-wm8804-audio
-
-# Enable JustBoom DAC and AMP Card
-#dtoverlay=justboom-dac
-
-# Enable JustBoom Digi Cards
-#dtoverlay=justboom-digi
 EOT
 
 elif [ "$SoundCard" = "6" ]
 then
-echo "options snd_bcm2835 index=0" | sudo tee -a /etc/modprobe.d/alsa-base.conf
-sudo cat <<EOT >> /etc/asound.conf
-pcm.pulse {
-    type pulse
-    card 1
-}
-ctl.pulse {
-    type pulse
-    card 1
-}
-
-pcm.!default {
-    type hw
-    card 1
-}
-ctl.!default {
-    type hw
-    card 1
-}
-EOT
-sudo cat <<EOT >>/boot/config.txt
-# Enable HiFiberry Amp
-#dtoverlay=hifiberry-amp
-
-# Enable HiFiberry DAC Light
-#dtoverlay=hifiberry-dac
-
-# Enable HiFiberry DAC Standard/Pro
-#dtoverlay=hifiberry-dacplus
-
-
-# Enable HiFiberry Digi
-#dtoverlay=hifiberry-digi
-
-# Enable iqaudio-dac
-#dtoverlay=iqaudio-dac
-
-# Enable iqaudio-dat+
+cat <<EOT >>/boot/config.txt
+# Enable iqaudio-dac+
 dtoverlay=iqaudio-dacplus
-
-# Enable iqaudio-DigiAMP+
-#dtoverlay=iqaudio-dacplus,unmute_amp
-
-# Enable iqaudio-Digi+
-#dtoverlay=iqaudio-digi-wm8804-audio
-
-# Enable JustBoom DAC and AMP Card
-#dtoverlay=justboom-dac
-
-# Enable JustBoom Digi Cards
-#dtoverlay=justboom-digi
 EOT
 elif [ "$SoundCard" = "7" ]
 then
-echo "options snd_bcm2835 index=0" | sudo tee -a /etc/modprobe.d/alsa-base.conf
-sudo cat <<EOT >> /etc/asound.conf
-pcm.pulse {
-    type pulse
-    card 1
-}
-ctl.pulse {
-    type pulse
-    card 1
-}
-
-pcm.!default {
-    type hw
-    card 1
-}
-ctl.!default {
-    type hw
-    card 1
-}
-EOT
-sudo cat <<EOT >>/boot/config.txt
-# Enable HiFiberry Amp
-#dtoverlay=hifiberry-amp
-
-# Enable HiFiberry DAC Light
-#dtoverlay=hifiberry-dac
-
-# Enable HiFiberry DAC Standard/Pro
-#dtoverlay=hifiberry-dacplus
-
-
-# Enable HiFiberry Digi
-#dtoverlay=hifiberry-digi
-
-# Enable iqaudio-dac
-#dtoverlay=iqaudio-dac
-
-# Enable iqaudio-dac+
-#dtoverlay=iqaudio-dacplus
-
-# Enable iqaudio-Digi+
-#dtoverlay=iqaudio-digi-wm8804-audio
-
+cat <<EOT >>/boot/config.txt
 # Enable iqaudio-DigiAMP+
 dtoverlay=iqaudio-dacplus,unmute_amp
-
-# Enable JustBoom DAC and AMP Card
-#dtoverlay=justboom-dac
-
-# Enable JustBoom Digi Cards
-#dtoverlay=justboom-digi
 EOT
 
 elif [ "$SoundCard" = "8" ]
 then
 echo "options snd_bcm2835 index=0" | sudo tee -a /etc/modprobe.d/alsa-base.conf
-sudo cat <<EOT >> /etc/asound.conf
-pcm.pulse {
-    type pulse
-    card 1
-}
-ctl.pulse {
-    type pulse
-    card 1
-}
-
-pcm.!default {
-    type hw
-    card 1
-}
-ctl.!default {
-    type hw
-    card 1
-}
-EOT
-sudo cat <<EOT >>/boot/config.txt
-# Enable HiFiberry Amp
-#dtoverlay=hifiberry-amp
-
-# Enable HiFiberry DAC Light
-#dtoverlay=hifiberry-dac
-
-# Enable HiFiberry DAC Standard/Pro
-#dtoverlay=hifiberry-dacplus
-
-
-# Enable HiFiberry Digi
-#dtoverlay=hifiberry-digi
-
-# Enable iqaudio-dac
-#dtoverlay=iqaudio-dac
-
-# Enable iqaudio-dac+
-#dtoverlay=iqaudio-dacplus
-
-# Enable iqaudio-DigiAMP+
-#dtoverlay=iqaudio-dacplus,unmute_amp
-
+cat <<EOT >>/boot/config.txt
 # Enable iqaudio-Digi+
 dtoverlay=iqaudio-digi-wm8804-audio
-
-# Enable JustBoom DAC and AMP Card
-#dtoverlay=justboom-dac
-
-# Enable JustBoom Digi Cards
-#dtoverlay=justboom-digi
-
-
-EOT
-elif [ "$SoundCard" = "9" ]
-then
-echo "options snd_bcm2835 index=0" | sudo tee -a /etc/modprobe.d/alsa-base.conf
-sudo cat <<EOT >> /etc/asound.conf
-pcm.pulse {
-    type pulse
-    card 1
-}
-ctl.pulse {
-    type pulse
-    card 1
-}
-
-pcm.!default {
-    type hw
-    card 1
-}
-ctl.!default {
-    type hw
-    card 1
-}
-EOT
-sudo cat <<EOT >>/boot/config.txt
-# Enable HiFiberry Amp
-#dtoverlay=hifiberry-amp
-
-# Enable HiFiberry DAC Light
-#dtoverlay=hifiberry-dac
-
-# Enable HiFiberry DAC Standard/Pro
-#dtoverlay=hifiberry-dacplus
-
-
-# Enable HiFiberry Digi
-#dtoverlay=hifiberry-digi
-
-# Enable iqaudio-dac
-#dtoverlay=iqaudio-dac
-
-# Enable iqaudio-dac+
-#dtoverlay=iqaudio-dacplus
-
-# Enable iqaudio-DigiAMP+
-#dtoverlay=iqaudio-dacplus,unmute_amp
-
-# Enable iqaudio-Digi+
-#dtoverlay=iqaudio-digi-wm8804-audio
-
-# Enable JustBoom DAC and AMP Card
-#dtoverlay=justboom-dac
-
-# Enable JustBoom Digi Cards
-#dtoverlay=justboom-digi
 EOT
 elif [ "$SoundCard" = "10" ]
 then
-echo "options snd_bcm2835 index=0" | sudo tee -a /etc/modprobe.d/alsa-base.conf
-sudo cat <<EOT >> /etc/asound.conf
-pcm.pulse {
-    type pulse
-    card 1
-}
-ctl.pulse {
-    type pulse
-    card 1
-}
-
-pcm.!default {
-    type hw
-    card 1
-}
-ctl.!default {
-    type hw
-    card 1
-}
-EOT
-exc sudo cat <<EOT >>/boot/config.txt
-# Enable HiFiberry Amp
-#dtoverlay=hifiberry-amp
-
-# Enable HiFiberry DAC Light
-#dtoverlay=hifiberry-dac
-
-# Enable HiFiberry DAC Standard/Pro
-#dtoverlay=hifiberry-dacplus
-
-
-# Enable HiFiberry Digi
-#dtoverlay=hifiberry-digi
-
-# Enable iqaudio-dac
-#dtoverlay=iqaudio-dac
-
-# Enable iqaudio-dac+
-#dtoverlay=iqaudio-dacplus
-
-# Enable iqaudio-DigiAMP+
-#dtoverlay=iqaudio-dacplus,unmute_amp
-
-# Enable iqaudio-Digi+
-#dtoverlay=iqaudio-digi-wm8804-audio
-
+cat <<EOT >>/boot/config.txt
 # Enable JustBoom DAC and AMP Card
 dtoverlay=justboom-dac
-
-# Enable JustBoom Digi Cards
-#dtoverlay=justboom-digi
 EOT
 elif [ "$SoundCard" = "11" ]
 then
-echo "options snd_bcm2835 index=0" | sudo tee -a /etc/modprobe.d/alsa-base.conf
-sudo cat <<EOT >> /etc/asound.conf
-pcm.pulse {
-    type pulse
-    card 1
-}
-ctl.pulse {
-    type pulse
-    card 1
-}
-
-pcm.!default {
-    type hw
-    card 1
-}
-ctl.!default {
-    type hw
-    card 1
-}
-EOT
-sudo cat <<EOT >>/boot/config.txt
-# Enable HiFiberry Amp
-#dtoverlay=hifiberry-amp
-
-# Enable HiFiberry DAC Light
-#dtoverlay=hifiberry-dac
-
-# Enable HiFiberry DAC Standard/Pro
-#dtoverlay=hifiberry-dacplus
-
-# Enable HiFiberry Digi
-#dtoverlay=hifiberry-digi
-
-# Enable iqaudio-dac
-#dtoverlay=iqaudio-dac
-
-# Enable iqaudio-dac+
-#dtoverlay=iqaudio-dacplus
-
-# Enable iqaudio-DigiAMP+
-#dtoverlay=iqaudio-dacplus,unmute_amp
-
-# Enable iqaudio-Digi+
-#dtoverlay=iqaudio-digi-wm8804-audio
-
-# Enable JustBoom DAC and AMP Card
-#dtoverlay=justboom-dac
-
+cat <<EOT >>/boot/config.txt
 # Enable JustBoom Digi Cards
 dtoverlay=justboom-digi
 EOT
