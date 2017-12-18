446ab49b973b25ec087d3a059675920f84022ad1
Brett Reinhard <brettreinhard@gmail.com>
2017-12-18 10:02:15 -0800

    updated kodi config, prevent any errors

--- kodi_config.sh
+++ kodi_config.sh
@@ -9,16 +9,7 @@ exc usermod -a -G input kodi
 exc usermod -a -G lp kodi
 exc usermod -a -G pulse-access,audio root
 exc adduser kodi pulse-access
-exc patch /etc/default/kodi << EOT
-@@ -1,5 +1,5 @@
- # Set this to 1 to enable startup
--ENABLED=0
-+ENABLED=1
- 
- # The user to run Kodi as
- USER=kodi
-
-EOT
+exc sed -i "s/ENABLED=0/ENABLED=1/" /etc/default/kodi 
 # Add gpu_mem=256 to config.txt for use with Kodi
 echo gpu_mem=256 >> /boot/config.txt
 # Faster Boot
