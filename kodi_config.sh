6a9363648945ff906866aec6af76f3c49acc5733
Brett Reinhard <brettreinhard@gmail.com>
2017-12-18 11:26:59 -0800

    add user before

--- kodi_config.sh
+++ kodi_config.sh
@@ -5,10 +5,11 @@ then
     source functions.sh
     source dependencies.sh
 fi
+exc adduser kodi pulse-access
 exc usermod -a -G input kodi
 exc usermod -a -G lp kodi
 exc usermod -a -G pulse-access,audio root
-exc adduser kodi pulse-access
+
 exc sed -i "s/ENABLED=0/ENABLED=1/" /etc/default/kodi 
 # Add gpu_mem=256 to config.txt for use with Kodi
 echo gpu_mem=256 >> /boot/config.txt
